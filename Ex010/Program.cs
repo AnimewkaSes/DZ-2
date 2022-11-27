b7f71147606c3efa8c903c0acf98f412ab2c9143
AnimewkaSes <brol0526@gmail.com>
2022-11-27 22:37:07 +0600

    Задание 10

--- Ex010/Program.cs
+++ Ex010/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Программа для нахождения вторую цифру трёх значного числа \n");
+char rest;
+do{
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = number.ToString();
+Console.Write("Вторая цифра: ");
+Console.WriteLine(str[1]);
+Console.Write("Для продолжения введите y, для завершения любая клавиша: ");
+rest = Convert.ToChar(Console.ReadLine());
+}
+while (rest =='y');
