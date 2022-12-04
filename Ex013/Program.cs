627d343985864af55351bc7e7adb067e3bdb9f10
AnimewkaSes <brol0526@gmail.com>
2022-12-04 17:50:36 +0600

    Ex013

--- Ex013/Program.cs
+++ Ex013/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Программа для нахождения вторую цифру трёх значного числа \n");
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numDig = NumberDigit(number);
+
+if (numDig<=2)
+{
+  Console.Write("Третьей цифры нет,"); 
+   
+}
+else{
+if (numDig>3)
+{
+    number = number/Convert.ToInt32(Math.Pow(10,numDig-3));
+}
+number = number %10;
+ Console.WriteLine($"Третья цифра введённого числа: {number}");
+}
+
+
+int NumberDigit(int number){
+int count = 0;
+while (number>0)
+{
+    number=number/10;
+    count ++;
+}
+ return count;
+}
+   
+
+
+
